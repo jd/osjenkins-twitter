12a3ae3bd8d0dc3fb603521bc661ef198ecb1671
Julien Danjou <julien@danjou.info>
2014-10-01 15:56:19 +0200

    Handle empty queue, more symbols

--- osjktw/__init__.py
+++ osjktw/__init__.py
@@ -6,35 +6,58 @@ import time
 import requests
 import twitter
 
+
 def tweet():
     with open("credentials.json") as f:
         creds = json.load(f)
 
-    req = requests.get("http://zuul.openstack.org/status.json")
-    content = json.loads(req.content)
-
-    for pipeline in content['pipelines']:
-        if pipeline['name'] == 'gate':
-            count = sum([sum(map(len, change['heads']))
-                         for change in pipeline['change_queues']])
-            for change in pipeline['change_queues']:
-                # Queue name
-                if change['name'] == 'integrated':
-                    hours = ((time.time() * 1000 - change['heads'][0][0]['enqueue_time']) / (3600 * 1000))
-                    break
-            break
-
-    if hours > 4:
-        symbol = "😱"
-    elif hours > 2:
-        symbol = "😓"
-    else:
-        symbol = "😎"
-
     api = twitter.Api(**creds)
 
-    status = api.PostMedia('Gate status %s\n'
-                           '%d patches in queue\n'
-                           'Integrated queue delay: %.1f hours\n'
-                           % (symbol, count, hours),
-    'http://graphite.openstack.org/render/?from=-8hours&width=500&height=200&margin=10&hideLegend=true&hideAxes=false&hideGrid=false&target=color(stats.gauges.zuul.pipeline.gate.current_changes,%20%276b8182%27)')
+    try:
+        req = requests.get("http://zuul.openstack.org/status.json")
+        content = json.loads(req.content)
+
+        for pipeline in content['pipelines']:
+            if pipeline['name'] == 'gate':
+                count = sum([sum(map(len, change['heads']))
+                             for change in pipeline['change_queues']])
+                if count:
+                    for change in pipeline['change_queues']:
+                        # Queue name
+                        if change['name'] == 'integrated':
+                            hours = ((time.time() * 1000
+                                      - change['heads'][0][0]['enqueue_time'])
+                                     / (3600 * 1000))
+                            break
+                break
+    except Exception:
+        api.PostUpdate("Where is the gate!? 😱")
+        # Re raise to have the full trace
+        raise
+
+    if count:
+        if hours > 12:
+            symbol = "😰"
+        elif hours > 10:
+            symbol = "😤"
+        elif hours > 8:
+            symbol = "😓"
+        elif hours > 4:
+            symbol = "😣"
+        elif hours > 2:
+            symbol = "😠"
+        else:
+            symbol = "😏"
+
+        text = '%d patches in queue %s\n'
+        'Integrated queue delay: %.1f hours\n' % (count, symbol, hours)
+    else:
+        text = "The gate is empty 😎 🏖"
+
+    api.PostMedia(
+        text,
+        'http://graphite.openstack.org/render/'
+        '?from=-8hours&width=500&height=200'
+        '&margin=10&hideLegend=true&hideAxes=false&hideGrid=false'
+        '&target=color(stats.gauges.zuul.pipeline.gate.current_changes,'
+        '%20%276b8182%27)')
